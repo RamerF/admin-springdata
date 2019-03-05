c570eafe089cc9eed835554d16b6a44761c69c8a
tangxf <txf123>
2019-03-05 18:39:55 +0800

    添加: service层新增通用保存/更新方法.

--- src/main/java/org/ramer/admin/entity/pojo/AbstractEntityPoJo.java
+++ src/main/java/org/ramer/admin/entity/pojo/AbstractEntityPoJo.java
@@ -17,23 +17,6 @@ public abstract class AbstractEntityPoJo {
   private Date createTime;
   private Date updateTime;
 
-  /**
-   * 复制通用字段值.
-   *
-   * @param t PoJo实体
-   * @param e Domain实体
-   * @param <T> PoJo实体
-   * @param <E> Domain实体
-   * @return PoJo实体
-   */
-  public static <T extends AbstractEntityPoJo, E extends AbstractEntity> T of(T t, E e) {
-    t.setId(e.getId());
-    t.setState(e.getState());
-    t.setUpdateTime(e.getUpdateTime());
-    t.setCreateTime(e.getCreateTime());
-    return t;
-  }
-
   public <E extends AbstractEntity, T extends AbstractEntityPoJo> T of(E entity, Class<T> clazz) {
     if (Objects.isNull(entity)) {
       return null;
