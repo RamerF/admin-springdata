9652b1c76d7c170faa90ac7badd6656f782aaa3c
RamerF <1390635973@qq.com>
2019-03-08 13:08:21 +0800

    添加: 拦截请求方式不支持.

--- src/main/java/org/ramer/admin/config/GlobalExceptionHandler.java
+++ src/main/java/org/ramer/admin/config/GlobalExceptionHandler.java
@@ -1,17 +1,18 @@
 package org.ramer.admin.config;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.ramer.admin.entity.domain.manage.ManageLog;
 import org.ramer.admin.entity.response.CommonResponse;
 import org.ramer.admin.exception.CommonException;
 import org.ramer.admin.service.manage.system.ManageLogService;
 import org.ramer.admin.service.manage.system.ManagerService;
 import org.ramer.admin.util.IpUtils;
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -46,6 +47,9 @@ public class GlobalExceptionHandler {
       log.error(exception.getMessage(), exception);
       request.setAttribute("error", exception.getMessage());
       return CommonResponse.fail(exception.getMessage());
+    }
+    if (exception instanceof HttpRequestMethodNotSupportedException) {
+      return CommonResponse.fail("请求方式不支持");
     }
     log.error(exception.getMessage(), exception);
     return CommonResponse.fail("系统繁忙,请稍后再试");
